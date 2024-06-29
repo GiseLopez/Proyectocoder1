92f46fd75230f6eb31fba91c8d125b57d095d653
Gisele E. López <giseleelopez@gmail.com>
2024-06-29 20:51:57 -0300

    estoy en la rama gisele

--- ejemplo1.py
+++ ejemplo1.py
@@ -13,3 +13,6 @@ def mensaje2():
 def multiplicar(n1,n2):
     print("el resultado de la multiplicacion entre n1 y n2 es de: ", n1*n2)
 sumar(int(input("ingresa el primer numero")),int(input("ingresa el primer numero")))
+
+def mensaje3():
+    print("estoy en la rama gisele")
