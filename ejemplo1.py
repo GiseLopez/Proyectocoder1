c3bf02f0d15f09639d6db6aea90cb730ba20a71c
Gisele E. López <giseleelopez@gmail.com>
2024-06-29 20:28:07 -0300

    ingreso la funcion multiplicar en archivo ejemplo1 y dividir en el archivo ejemplo2

--- ejemplo1.py
+++ ejemplo1.py
@@ -6,3 +6,6 @@ mensaje()
 def sumar(n1,n2):
     print("el resultado de la suma entre n1 y n2 es de: ", n1+n2)
 sumar(int(input("ingresa el primer numero")),int(input("ingresa el primer numero")))
+def multiplicar(n1,n2):
+    print("el resultado de la multiplicacion entre n1 y n2 es de: ", n1*n2)
+sumar(int(input("ingresa el primer numero")),int(input("ingresa el primer numero")))
--- ejemplo2.py
+++ ejemplo2.py
@@ -1,3 +1,6 @@
 def restar(n1,n2):
     print("el resultado de la resta entre n1 y n2 es de: ", n1-n2)
-sumar(int(input("ingresa el primer numero")),int(input("ingresa el primer numero")))
+restar(int(input("ingresa el primer numero")),int(input("ingresa el primer numero")))
+def division(n1,n2):
+    print("el resultado de la division entre n1 y n2 es de: ", n1/n2)
+division(int(input("ingresa el primer numero")),int(input("ingresa el primer numero")))
