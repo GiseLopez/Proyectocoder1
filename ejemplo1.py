30fe9249645b344bc219dcd25a87f1e8cb3cd7e6
Gisele E. López <giseleelopez@gmail.com>
2024-06-29 20:46:30 -0300

    regresé del pasado

--- ejemplo1.py
+++ ejemplo1.py
@@ -6,6 +6,10 @@ mensaje()
 def sumar(n1,n2):
     print("el resultado de la suma entre n1 y n2 es de: ", n1+n2)
 sumar(int(input("ingresa el primer numero")),int(input("ingresa el primer numero")))
+
+def mensaje2():
+    print("volvi al pasado")
+    
 def multiplicar(n1,n2):
     print("el resultado de la multiplicacion entre n1 y n2 es de: ", n1*n2)
 sumar(int(input("ingresa el primer numero")),int(input("ingresa el primer numero")))
